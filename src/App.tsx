e45cc99f2b7053f823a05b23de11de34afef14aa
Sergio <s.albercaalberca@gmail.com>
2020-11-12 22:13:58 +0100

    principal page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./views/dashboard/dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
